03f556e9da6afa76bbfb116acbb51d3c15038d49
sds131 <871145614@qq.com>
2021-09-17 23:27:59 +0800

    commit

---
+++ util/generate-aliases.py
@@ -0,0 +1,44 @@
+import gzip
+from csrankings import *
+
+facultydict = {}
+with open("faculty-affiliations.csv") as f:
+    rdr = csv.DictReader(f)
+    for row in rdr:
+        facultydict[row["name"]] = row["affiliation"]
+
+def parseDBLP(facultydict):
+    counter = 0
+
+    # with open('dblp.xml', mode='r') as f:
+        
+    with gzip.open('dblp.xml.gz-full', mode='rb') as f:
+
+        oldnode = None
+        
+        for (event, node) in ElementTree.iterparse(f, events=['start', 'end']):
+
+            if (oldnode is not None):
+                oldnode.clear()
+            oldnode = node
+            authors = 0
+            authorList = []
+
+            # print((node.tag))
+            if (node.tag == 'www'):
+                for child in node:
+                    if (child.tag == 'author'):
+                        authorName = child.text
+                        if (authorName is not None):
+                            authorName = authorName.strip()
+                            authors = authors + 1
+                            authorList.append(authorName.encode('utf-8'))
+                if (authors > 1):
+                    pairs = [(authorList[0],item) for item in authorList[1:]]
+                    for p in pairs:
+                        print(p[1].decode('utf-8') + "," + p[0].decode('utf-8'))
+                
+
+parseDBLP(facultydict)
+
+
